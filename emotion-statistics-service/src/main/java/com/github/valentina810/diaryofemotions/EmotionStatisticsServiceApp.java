40308fcc944d729a282d7635ebd98f48b0d2b510
Колесникова Валентина Валерьевна <v.kolesnikova@vtb.ru>
2023-05-09 23:04:51 +0300

    feat: добавлен каркас приложения

---
+++ emotion-statistics-service/src/main/java/com/github/valentina810/diaryofemotions/EmotionStatisticsServiceApp.java
@@ -0,0 +1,12 @@
+package com.github.valentina810.diaryofemotions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionStatisticsServiceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionStatisticsServiceApp.class, args);
+	}
+}
