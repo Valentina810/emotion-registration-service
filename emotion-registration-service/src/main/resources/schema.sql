b5e355324044aa9a2bcb71372c69ead5ab9d939d
valentinakolesnikova <polikarpova-v@yandex.ru>
2023-05-29 23:14:53 +0300

    feat: добавить таблицы для регистриции множества причин и принимаемых лекарств для одного события

--- emotion-registration-service/src/main/resources/schema.sql
+++ emotion-registration-service/src/main/resources/schema.sql
@@ -9,12 +9,19 @@ CREATE TABLE IF NOT EXISTS users
     CONSTRAINT uq_users_email UNIQUE (email)
 );
 
+CREATE TABLE IF NOT EXISTS emotions
+(
+    id_emotion bigint                 NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name       character varying(250) NOT NULL,
+    CONSTRAINT emotions_pkey PRIMARY KEY (id_emotion),
+    CONSTRAINT uq_emotion_name UNIQUE (name)
+);
+
 CREATE TABLE IF NOT EXISTS events
 (
     id_event    bigint                      NOT NULL GENERATED ALWAYS AS IDENTITY,
     user_id     bigint                      NOT NULL NOT NULL,
     emotion_id  bigint                      NOT NULL NOT NULL,
-    cause_id    bigint                      NOT NULL NOT NULL,
     description character varying(5000),
     created_on  timestamp without time zone,
     start_date  timestamp without time zone NOT NULL,
@@ -23,17 +30,7 @@ CREATE TABLE IF NOT EXISTS events
     CONSTRAINT user_id_id_user FOREIGN KEY (user_id)
         REFERENCES users (id_user),
     CONSTRAINT emotion_id_id_emotion FOREIGN KEY (emotion_id)
-        REFERENCES emotions (id_emotion),
-    CONSTRAINT cause_id_id_cause FOREIGN KEY (cause_id)
-        REFERENCES causes (id_cause)
-);
-
-CREATE TABLE IF NOT EXISTS emotions
-(
-    id_emotion bigint                 NOT NULL GENERATED ALWAYS AS IDENTITY,
-    name       character varying(250) NOT NULL,
-    CONSTRAINT emotions_pkey PRIMARY KEY (id_emotion),
-    CONSTRAINT uq_emotion_name UNIQUE (name)
+        REFERENCES emotions (id_emotion)
 );
 
 CREATE TABLE IF NOT EXISTS causes
@@ -44,10 +41,22 @@ CREATE TABLE IF NOT EXISTS causes
     CONSTRAINT uq_causes_name UNIQUE (name)
 );
 
+CREATE TABLE IF NOT EXISTS events_causes
+(
+    event_id       bigint REFERENCES events (id_event) ON DELETE CASCADE,
+    cause_id bigint REFERENCES causes (id_cause) ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS tablets
 (
     id_tablet bigint                 NOT NULL GENERATED ALWAYS AS IDENTITY,
     name      character varying(250) NOT NULL,
     CONSTRAINT tablets_pkey PRIMARY KEY (id_tablet),
     CONSTRAINT uq_tablets_name UNIQUE (name)
+);
+
+CREATE TABLE IF NOT EXISTS events_tablets
+(
+    event_id       bigint REFERENCES events (id_event) ON DELETE CASCADE,
+    tablet_id bigint REFERENCES tablets (id_tablet) ON DELETE CASCADE
 );
